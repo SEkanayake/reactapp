2b826c520d6221859cda68cbead970a4195607b1
Sarala Ekanayake <36910539+SEkanayake@users.noreply.github.com>
2019-02-26 21:03:11 +0530

    Create a frontent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className = "container">
+    <div className="row">
+    <div className="col-md-8">
+    <form className="form-horizontal">
+      <fieldset>
+
+      <legend>Nearby places</legend>
+        <div className="col-md-10">
+        
+        
+        <div className="form-group">
+      <label htmlfor="exampleInputEmail1">Places</label>
+      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search here"/>
+      
+    </div>
+    </div>
+    <div className="col-md-2">
+      <button className="btn btn-primary">Search</button>
+    </div>
+    </fieldset>  
+  </form>
+  <ul>
+    <li>Kandy</li>
+    <li>Colombo</li>
+  </ul>
+  </div>
+  <div className="col-md-4">
+  
+  <h2>saved places</h2>
+  
+  </div>
+  </div>
+  </div>
     );
   }
 }
